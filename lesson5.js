dfc2b7e5a63d48c9df2a2b4950941af4f5eaf40c
tanya <tk@prezentor.com>
2025-02-02 13:58:23 +0200

    for

--- lesson5.js
+++ lesson5.js
@@ -1,23 +1,39 @@
+// task
+function pow(x, y) {
+    var result = 1;
+    for (var i = 0; i < y; i++)
+      result = result * x;
+    return result;
+  };
+  let homework3 = pow(6, 7);
+  console.log(homework3);
+
+
 
 // declaration function
-function pow(a, b) {
-    return a ** b;
- };
-let homework = pow(3,4);
-console.log(homework);
+// function pow1(a, b) {
+//     return a ** b;
+//  };
+// let homework = pow1(3,4);
+// console.log(homework);
   
+// const target = 3;
+// let sum = 4;
+
 
 //expression function
-const pow1 = function (x,y) {
-    return x ** y;
-};
-let homework1 = pow1(5,6);
-console.log(homework1);
+// const pow2 = function (x,y) {
+//     return x ** y;
+// };
+// let homework1 = pow2(5,6);
+// console.log(homework1);
 
 //стрілочна функція
 
-const pow2 = (x,y) => {
-    return x ** y;
-};
-let homework2 = pow2(2,4);
-console.log(homework2);
+// const pow3 = (x,y) => {
+//     return x ** y;
+// };
+// let homework2 = pow3(2,4);
+// console.log(homework2);
+
+
